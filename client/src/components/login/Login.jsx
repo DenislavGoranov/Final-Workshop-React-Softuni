64ef661c682ce32aee39c64ace26fc0ea809d874
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-15 15:14:20 +0300

    update login component

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,18 +1,18 @@
 export default function Login() {
     return (
-        <section id="login-page" class="auth">
+        <section id="login-page" className="auth">
             <form id="login">
 
-                <div class="container">
-                    <div class="brand-logo"></div>
+                <div className="container">
+                    <div className="brand-logo"></div>
                     <h1>Login</h1>
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" autoComplete="email" />
 
-                    <label for="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" class="btn submit" value="Login" />
-                    <p class="field">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" autoComplete="current-password" />
+                    <input type="submit" className="btn submit" value="Login" />
+                    <p className="field">
                         <span>If you don't have profile click <a href="#">here</a></span>
                     </p>
                 </div>
