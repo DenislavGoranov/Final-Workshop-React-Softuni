c96f0826e98c7ec10ed725cbf5135caaec9313f0
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-15 15:14:26 +0300

    update register component

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,23 +1,23 @@
 export default function Register() {
     return (
-        <section id="register-page" class="content auth">
+        <section id="register-page" className="content auth">
             <form id="register">
-                <div class="container">
-                    <div class="brand-logo"></div>
+                <div className="container">
+                    <div className="brand-logo"></div>
                     <h1>Register</h1>
 
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="maria@email.com" autoComplete="email" />
 
-                    <label for="pass">Password:</label>
-                    <input type="password" name="password" id="register-password" />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" autoComplete="new-password" />
 
-                    <label for="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+                    <label htmlFor="confirm-password">Confirm Password:</label>
+                    <input type="password" name="confirm-password" id="confirm-password" autoComplete="confirm-password" />
 
-                    <input class="btn submit" type="submit" value="Register" />
+                    <input className="btn submit" type="submit" value="Register" />
 
-                    <p class="field">
+                    <p className="field">
                         <span>If you already have profile click <a href="#">here</a></span>
                     </p>
                 </div>
