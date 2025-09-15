718ce830a046398fdde9c539368b7196f215f1cb
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-15 22:07:22 +0300

    add login functionality

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,18 +1,48 @@
-import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { Link, useNavigate, } from "react-router-dom";
 export default function Login() {
+
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const submitFormHandler = async (e) => {
+        e.preventDefault();
+
+        await fetch(`http://localhost:3030/users/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
+
+        console.log('Logged in: ', userData)
+
+        navigate(`/`);
+    }
+
+    const onChange = (e) => {
+        setUserData((oldValues) => ({
+            ...oldValues,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
     return (
         <section id="login-page" className="auth">
-            <form id="login">
+            <form id="login" onSubmit={submitFormHandler}>
 
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" autoComplete="email" />
+                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" autoComplete="email" onChange={onChange} value={userData.email} />
 
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" autoComplete="current-password" />
+                    <input type="password" id="password" name="password" autoComplete="current-password" onChange={onChange} value={userData.password} />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <Link to='/register'>here</Link></span>
