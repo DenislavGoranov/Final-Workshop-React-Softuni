cdc607f2c328801d08aec7507da89fd1994de330
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-15 15:43:18 +0300

    create game layout

---
+++ client/src/components/game-layout/GameLayout.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function GameLayout({ game }) {
+    return (
+
+        <div className="game">
+            <div className="image-wrap">
+                <img src={game.imageUrl} />
+            </div>
+            <h3>Cover Fire</h3>
+            <div className="rating">
+                <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+            </div>
+            <div className="data-buttons">
+                <Link to="#" className="btn details-btn">Details</Link>
+            </div>
+        </div>
+    );
+}
