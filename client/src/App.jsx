ba02a7ab77e826b09a9ac8f539697cb63a1f92cd
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-15 15:08:32 +0300

    add login/register routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import CatalogPage from "./components/catalog-page/CatalogPage";
 import CreatePage from "./components/create-page/CreatePage";
 import Header from "./components/header/Header";
 import HomePage from "./components/home-page/HomePage";
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 import { Route, Routes } from 'react-router-dom';
 
@@ -15,6 +17,8 @@ export default function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='games' element={<CatalogPage />} />
                 <Route path='create' element={<CreatePage />} />
+                <Route path='login' element={<Login />} />
+                <Route path='register' element={<Register />} />
             </Routes>
 
             <main id="main-content">
