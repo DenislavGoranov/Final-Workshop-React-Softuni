73da4ef613d08a7daa06a6c01cb29a554da6e69f
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-02 16:08:50 +0300

    remove async from the requester

--- client/src/api/games-api.js
+++ client/src/api/games-api.js
@@ -2,14 +2,14 @@ import requester from "./requester"
 
 const BASE_URL = 'http://localhost:3030/data/games'
 
-export const getAll = async () => {
-    const result = await requester.get(`${BASE_URL}/?sortBy=_createdOn%20desc`);
+export const getAll = () => {
+    const result = requester.get(`${BASE_URL}/?sortBy=_createdOn%20desc`);
 
     return result;
 }
 
-export const getRecent = async () => {
-    const result = await requester.get(`${BASE_URL}/?sortBy=_createdOn%20desc&distinct=category`);
+export const getRecent = () => {
+    const result = requester.get(`${BASE_URL}/?sortBy=_createdOn%20desc&distinct=category`);
 
     return result;
 }
