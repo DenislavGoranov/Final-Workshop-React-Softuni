b1898f1b9b9ada15f2ab5a29240d3c18c56c0cce
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-02 16:14:11 +0300

    update game layout

--- client/src/components/home-page/game-layout-home/GameLayoutHome.jsx
+++ client/src/components/home-page/game-layout-home/GameLayoutHome.jsx
@@ -7,7 +7,7 @@ export default function GameLayout({ game }) {
             <div className="image-wrap">
                 <img src={game.imageUrl} />
             </div>
-            <h3>Cover Fire</h3>
+            <h3>{game.title}</h3>
             <div className="rating">
                 <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
             </div>
