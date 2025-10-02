197c09a8a237d655d3d196a967ac7c18183d6418
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-02 16:14:18 +0300

    import getOne

--- client/src/components/details-page/DetailsPage.jsx
+++ client/src/components/details-page/DetailsPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getOne } from "../../api/games-api";
 
 export default function DetailsPage() {
     const [gameData, setGameData] = useState({});
