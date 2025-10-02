4f6213bad944d8943502a06174cb2e2b5d06513b
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-02 16:17:40 +0300

    change title and category title tags

--- client/src/components/catalog-page/game-layout-catalog/GameLayoutCatalog.jsx
+++ client/src/components/catalog-page/game-layout-catalog/GameLayoutCatalog.jsx
@@ -5,8 +5,8 @@ export default function GameLayoutCatalog({ game }) {
         <div className="allGames">
             <div className="allGames-info">
                 <img src={game.imageUrl} />
-                <h6>{game.category}</h6>
                 <h2>{game.title}</h2>
+                <h4>{game.category}</h4>
                 <Link to={`/games/${game._id}`} className="details-button">Details</Link>
             </div>
 
