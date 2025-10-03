75d2c70d3f125f15f666a72a9450e25754e33703
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-03 14:55:13 +0300

    fix login component

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -2,7 +2,5 @@ import requester from "./requester";
 const BASE_URL = 'http://localhost:3030/users';
 
 export const login = async (email, password) => {
-    const authData = await requester.post(`${BASE_URL}/login`, {email, password});
-
-    return authData;
+    return requester.post(`${BASE_URL}/login`, {email, password});
 }
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,59 +1,46 @@
-import { useState } from "react";
-import { Link, useNavigate, } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import useForm from "../../hooks/useForm";
+import { useLogin } from "../../hooks/useAuth";
+
 export default function Login() {
-
     const navigate = useNavigate();
-    const [userData, setUserData] = useState({
-        email: '',
-        password: '',
-    });
-
-    const submitFormHandler = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await fetch(`http://localhost:3030/users/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(userData),
-            });
-
-            if (response.status == 403) {
-                throw new Error('Invalid User');
-            }
-
-            if (!response.ok) {
-                throw new Error('Login Failed');
-            }
-
-            console.log('Logged in: ', userData)
-            navigate(`/`);
-        } catch (err) {
-            console.log(err.message);
-        }
-    }
-
-    const onChange = (e) => {
-        setUserData((oldValues) => ({
-            ...oldValues,
-            [e.target.name]: e.target.value,
-        }));
-    }
+    const loginHandler = useLogin();
+    const { values, onChange, submitHandler } = useForm(
+        { email: '', password: '' },
+        async ({ email, password }) => {
+            await loginHandler(email, password);
+            navigate('/');
+        },
+    );
 
     return (
         <section id="login-page" className="auth">
-            <form id="login" onSubmit={submitFormHandler}>
+            <form id="login" onSubmit={submitHandler}>
 
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" autoComplete="email" onChange={onChange} value={userData.email} />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Sokka@gmail.com"
+                        autoComplete="email"
+                        onChange={onChange}
+                        value={values.email}
+                    />
 
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" autoComplete="current-password" onChange={onChange} value={userData.password} />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        autoComplete="current-password"
+                        onChange={onChange}
+                        value={values.password}
+                    />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <Link to='/register'>here</Link></span>
