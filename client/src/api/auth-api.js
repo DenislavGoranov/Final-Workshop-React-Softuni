78db91c2855c5e45ab683b2152029b455474fa9c
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-03 15:09:34 +0300

    fix the auth api

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -2,9 +2,13 @@ import requester from "./requester";
 const BASE_URL = 'http://localhost:3030/users';
 
 export const login = async (email, password) => {
-    return requester.post(`${BASE_URL}/login`, {email, password});
+    const result = await requester.post(`${BASE_URL}/login`, {email, password});
+
+    return result;
 }
 
 export const register = async (email, password) => {
-    return requester.post(`${BASE_URL}/register`, {email, password});
+    const result = await requester.post(`${BASE_URL}/register`, {email, password});
+
+    return result;
 }
