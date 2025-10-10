37230840e965321f535a396581cca364a7412bae
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-10 17:12:03 +0300

    add get all comments api

--- client/src/api/comment-api.js
+++ client/src/api/comment-api.js
@@ -2,6 +2,12 @@ import requester from "./requester"
 
 const BASE_URL = `http://localhost:3030/data/comments`
 
+export const getAll = (gameId) => {
+    const result = requester.get(`${BASE_URL}?where=gameId%3D%22${gameId}%22`);
+
+    return result;
+}
+
 export const create = (gameId, comment) => {
     const result = requester.post(BASE_URL, {gameId, comment});
 
