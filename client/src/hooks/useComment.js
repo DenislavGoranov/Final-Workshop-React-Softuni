90f5ddfd365d8b0a4b736520d987972ac58f2153
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-10-10 17:12:12 +0300

    add use comment hook

--- client/src/hooks/useComment.js
+++ client/src/hooks/useComment.js
@@ -1,4 +1,23 @@
-import { create } from "../api/comment-api";
+import { useEffect, useState } from "react";
+import { create, getAll } from "../api/comment-api";
+
+export const useGetAllComments = (gameId) => {
+    const [comments, setComments] = useState([]);
+    useEffect(() => {
+        const fetchComments = async () => {
+            try {
+                const result = await getAll(gameId);
+                setComments(result);
+            } catch (err) {
+            console.log(err.message);
+            }
+        }
+        if (gameId) {
+            fetchComments();
+        }
+    }, [gameId])
+    return comments;
+}
 
 export const useCreateComment = () => {
     const createComment = async (gameId, comment) => {
